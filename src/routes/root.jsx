17dea8727688365393b4c1121e6b28a5827d5343
张伟峰 <zhang.weifeng@tslsmart.com>
2024-10-09 11:28:45 +0800

    feat: 增加路由 action

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,12 @@
-import { Link, Outlet, useLoaderData } from "react-router-dom";
-import { getContacts } from "../contacts";
+import { Form, Link, Outlet, useLoaderData } from "react-router-dom";
+import { createContact, getContacts } from "../contacts";
+
+export async function action() {
+  const contact = await createContact();
+  return {
+    contact,
+  };
+}
 
 export async function loader() {
   const contacts = await getContacts();
@@ -10,6 +17,7 @@ export async function loader() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  console.log(contacts);
   return (
     <>
       <div id="sidebar">
@@ -26,9 +34,9 @@ export default function Root() {
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
           </form>
-          <form method="post">
+          <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
           {contacts.length ? (
